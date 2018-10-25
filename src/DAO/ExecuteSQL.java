96c6b2e13d9b07ffb2a67fe1c8904469f85c8e11
K3D50N <franciscokedson321@hotmail.com>
2018-10-25 14:33:28 -0300

    Class ExecuteSQL

---
+++ src/DAO/ExecuteSQL.java
@@ -0,0 +1,20 @@
+
+package DAO;
+
+import java.sql.Connection;
+
+public class ExecuteSQL {
+    private Connection con;
+    
+    public ExecuteSQL(Connection con){
+        setCon(con);
+    }
+    
+    public Connection getCon(){
+        return con;
+    }
+    
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+}
