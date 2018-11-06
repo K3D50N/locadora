19cdc0c01c59328b4b3a869ceea0b4227521b06a
K3D50N <franciscokedson321@hotmail.com>
2018-11-06 12:27:59 -0200

    listar cliente,Atualizar tabela,Pesquisar nome cliente,pesquisar cod cliente,Testar cliente,captura e alterar cliente

--- src/DAO/ClienteDAO.java
+++ src/DAO/ClienteDAO.java
@@ -8,7 +8,10 @@ package DAO;
 import Modelo.Cliente;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -46,4 +49,173 @@ public class ClienteDAO extends ExecuteSQL {
         }
     }
     
+    public List<Cliente> ListarCliente() throws SQLException{
+        String sql = "select idcliente,nome,rg,cpf,telefone,email from cliente";
+        List<Cliente> lista = new ArrayList<>();
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs != null){
+                while (rs.next()){
+                    Cliente a = new Cliente();
+                    a.setCodigo(rs.getInt(1));
+                    a.setNome(rs.getString(2));
+                    a.setRG(rs.getString(3));
+                    a.setCPF(rs.getString(4));
+                    a.setTelefone(rs.getString(5));
+                    a.setEmail(rs.getString(6));
+                    
+                    lista.add(a);
+                    
+                }
+                return lista;
+            }else{
+                return null;
+            }
+        } catch (SQLException e){
+            return null;
+        }
+    }
+    
+    public List<Cliente> Pesquisar_Nome_Cliente(String nome){
+        String sql = "select idclinete, nome, RG, CPF, Telefone, Email " 
+                + "from cliente where nome Like'" + nome +"%'";
+        List<Cliente> lista = new ArrayList<>();
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs != null){
+                while (rs.next()){
+                    Cliente a = new Cliente();
+                    a.setCodigo(rs.getInt(1));
+                    a.setNome(rs.getString(2));
+                    a.setRG(rs.getString(3));
+                    a.setCPF(rs.getString(4));
+                    a.setTelefone(rs.getString(5));
+                    a.setEmail(rs.getString(6));
+                    
+                    lista.add(a);
+                    
+                }
+                return lista;
+            }else{
+                return null;
+            }
+        } catch (SQLException e){
+            return null;
+        }
+        
+    }
+    
+        public List<Cliente> Pesquisar_Cod_Cliente(int cod){
+        String sql = "select idclinete, nome, RG, CPF, Telefone, Email " 
+                + "from cliente where idcliente'" + cod +"'";
+        List<Cliente> lista = new ArrayList<>();
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs != null){
+                while (rs.next()){
+                    Cliente a = new Cliente();
+                    a.setCodigo(rs.getInt(1));
+                    a.setNome(rs.getString(2));
+                    a.setRG(rs.getString(3));
+                    a.setCPF(rs.getString(4));
+                    a.setTelefone(rs.getString(5));
+                    a.setEmail(rs.getString(6));
+                    
+                    lista.add(a);
+                    
+                }
+                return lista;
+            }else{
+                return null;
+            }
+        } catch (SQLException e){
+            return null;
+        }
+        
+    }
+        
+        public boolean Testar_Cliente(int cod){
+            boolean Resultado = false;
+            try{
+            
+                String sql = "select * from cliente where idcliente = " + cod +"";
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                
+                if (rs != null){
+                    while (rs.next()) {
+                        Resultado = true;
+                        
+                    }
+                }
+            } catch (SQLException ex){
+                ex.getMessage();
+            }
+            return Resultado;
+        }
+        
+        public List<Cliente> CapturarCliente(int cod) throws SQLException{
+            String sql = "select * from cliente where idcliente =" + cod +" ";
+            List<Cliente> lista = new ArrayList<>();
+            try{
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ResultSet rs = ps.executeQuery();
+                if (rs != null){
+                    while (rs.next()){
+                    Cliente a = new Cliente();
+                    a.setCodigo(rs.getInt(1));
+                    a.setNome(rs.getString(2));
+                    a.setNascimento(rs.getString(3));
+                    a.setRG(rs.getString(4));
+                    a.setCPF(rs.getString(5));
+                    a.setEmail(rs.getString(6));
+                    a.setTelefone(rs.getString(7));
+                    a.setBairro(rs.getString(8));
+                    a.setRua(rs.getString(9));
+                    a.setNumero(rs.getInt(10));
+                    a.setCEP(rs.getString(11));
+                    lista.add(a);
+                 }
+                    return lista;
+                }else{
+                    return null;
+                }
+            } catch (SQLException e){
+                return null;
+            }
+        }
+        
+        public String Alterar_Cliente(Cliente a) throws SQLException{
+            String sql = "update cliente set nome = ? ,data_nasc = ?,rg = ?"
+                    + ",cpf = ?,email = ?,telefone = ?,bairro = ?,rua = ?,"
+                    + ",numero = ?,cep = ? where idcliente";
+            try{
+                PreparedStatement ps = getCon().prepareStatement(sql);
+                ps.setString(1, a.getNome());
+                ps.setString(2, a.getNascimento());
+                ps.setString(3, a.getRG());
+                ps.setString(4, a.getCPF());
+                ps.setString(5, a.getEmail());
+                ps.setString(6, a.getTelefone());
+                ps.setString(7, a.getBairro());
+                ps.setString(8, a.getRua());
+                ps.setInt(9, a.getNumero());
+                ps.setString(10, a.getCEP());
+                ps.setInt(11, a.getCodigo());
+                if (ps.executeUpdate() > 0){
+                    return "Atualizado com sucesso";
+                }else{
+                    return "Erro ao Atualizar";
+                }
+            } catch (SQLException e){
+                return e.getMessage();
+            }
+        }
 }
+
--- src/Visao/Consultar/ConsultarCliente.java
+++ src/Visao/Consultar/ConsultarCliente.java
@@ -6,6 +6,17 @@
 
 package Visao.Consultar;
 
+import DAO.ClienteDAO;
+import DAO.Conexao;
+import Modelo.Cliente;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Aluno
@@ -13,8 +24,12 @@ package Visao.Consultar;
 public class ConsultarCliente extends javax.swing.JFrame {
 
     /** Creates new form ConsultarCliente */
-    public ConsultarCliente() {
+    public ConsultarCliente() throws SQLException {
         initComponents();
+        
+        setTitle("Video Locadora");
+        setSize(970, 380);
+        AtualizaTable();
     }
 
     /** This method is called from within the constructor to
@@ -34,7 +49,7 @@ public class ConsultarCliente extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -47,6 +62,11 @@ public class ConsultarCliente extends javax.swing.JFrame {
         });
 
         jButton1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Aluno\\Documents\\NetBeansProjects\\Projeto_locadora\\src\\Imagens\\pesquisar.jpg")); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Pesquisa por código:");
 
@@ -57,6 +77,11 @@ public class ConsultarCliente extends javax.swing.JFrame {
         });
 
         jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\Aluno\\Documents\\NetBeansProjects\\Projeto_locadora\\src\\Imagens\\pesquisar.jpg")); // NOI18N
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Todos");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
@@ -65,7 +90,7 @@ public class ConsultarCliente extends javax.swing.JFrame {
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -73,7 +98,7 @@ public class ConsultarCliente extends javax.swing.JFrame {
                 "Código", "Cliente", "RG", "CPF", "Telefone", "Email"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTable);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -110,10 +135,10 @@ public class ConsultarCliente extends javax.swing.JFrame {
                         .addComponent(jLabel1)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jLabel2)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(21, 21, 21))
@@ -133,6 +158,16 @@ public class ConsultarCliente extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        BuscarNome();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        BuscarCod();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -164,7 +199,11 @@ public class ConsultarCliente extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ConsultarCliente().setVisible(true);
+                try {
+                    new ConsultarCliente().setVisible(true);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ConsultarCliente.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
@@ -176,9 +215,43 @@ public class ConsultarCliente extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
+
+    private void AtualizaTable() throws SQLException {
+       
+        Connection Con = Conexao.AbrirConexao();
+        ClienteDAO bd = new ClienteDAO(Con);
+        List<Cliente> lista = new ArrayList<>();
+        lista = bd.ListarCliente();
+        DefaultTableModel tbm =
+                (DefaultTableModel) jTable.getModel();
+        while (tbm.getRowCount() >0) {
+            tbm.removeRow(0);
+            
+        }
+        int i = 0;
+        for (Cliente tab : lista){
+            tbm.addRow(new String[1]);
+            jTable.setValueAt(tab.getCodigo(), i, 0);
+            jTable.setValueAt(tab.getNome(), i, 1);
+            jTable.setValueAt(tab.getRG(), i, 2);
+            jTable.setValueAt(tab.getCPF(), i, 3);
+            jTable.setValueAt(tab.getTelefone(), i, 4);
+            jTable.setValueAt(tab.getEmail(), i, 5);
+            i++;
+        }
+        Conexao.FecharConexao(Con);
+    }
+
+    private void BuscarNome() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void BuscarCod() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
